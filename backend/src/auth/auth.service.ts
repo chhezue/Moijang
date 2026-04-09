941677f59d4922422fe871ca4f57d6e9a88bfcc6
chhezue <chhezue@gmail.com>
2026-04-10 03:06:46 +0900

    Chore[#1] - 팀즈 로그인 관련 부분 삭제

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,13 +2,12 @@ import {
   Injectable,
   InternalServerErrorException,
   UnauthorizedException,
-} from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { JwtPayloadDto } from './dto/jwt-payload.dto';
-import { Request, Response } from 'express';
-import { ConfigService } from '@nestjs/config';
-import { TeamsAuthEndDto } from './dto/teams-auth-end.dto';
-import { UserService } from '../user/user.service';
+} from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { JwtPayloadDto } from "./dto/jwt-payload.dto";
+import { Request, Response } from "express";
+import { ConfigService } from "@nestjs/config";
+import { UserService } from "../user/user.service";
 
 @Injectable()
 export class AuthService {
@@ -18,64 +17,27 @@ export class AuthService {
     private readonly userService: UserService,
   ) {}
 
-  // 사용자 로그인 성공하면 JWT 토큰 생성
-  async login(user: TeamsAuthEndDto, res: Response): Promise<TeamsAuthEndDto> {
-    // JWT payload 생성
-    const payload: JwtPayloadDto = {
-      sub: user.id, // uuid
-      name: user.displayName, // displayName
-    };
-
-    // 토큰 생성
-    const accessToken = this.jwtService.sign(payload, { expiresIn: '5m' });
-    const refreshToken = this.jwtService.sign(payload, { expiresIn: '14d' });
-
-    // Response Header의 쿠키에 토큰 저장 -> 다음 요청 시 자동으로 쿠키 전송
-    this.setTokenCookies(res, accessToken, refreshToken);
-
-    // Response Body로 사용자 정보 반환(JSON 형태) -> 프론트가 직접 조작 가능
-    return user;
-  }
-
-  // Teams 인증 콜백 처리
-  async handleTeamsCallback(
-    query: any,
-    res: Response,
-  ): Promise<TeamsAuthEndDto> {
-    const currentUser = await this.userService.upsertUser(query);
-
-    // 로그인 처리 (JWT 토큰 생성 및 쿠키 설정)
-    const loginUser = await this.login(currentUser, res);
-    if (!loginUser) {
-      throw new InternalServerErrorException(
-        '로그인 처리 중 오류가 발생했습니다.',
-      );
-    }
-
-    return loginUser;
-  }
-
   // 생성된 쿠키를 응답의 cookie로 설정
   setTokenCookies(res: Response, accessToken: string, refreshToken: string) {
-    res.cookie('accessToken', accessToken, {
+    res.cookie("accessToken", accessToken, {
       httpOnly: true,
-      secure: this.configService.get<string>('NODE_ENV') === 'production',
-      sameSite: 'lax',
+      secure: this.configService.get<string>("NODE_ENV") === "production",
+      sameSite: "lax",
       maxAge: 5 * 60 * 1000, // 5분
     });
 
-    res.cookie('refreshToken', refreshToken, {
+    res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: this.configService.get<string>('NODE_ENV') === 'production',
-      sameSite: 'lax',
+      secure: this.configService.get<string>("NODE_ENV") === "production",
+      sameSite: "lax",
       maxAge: 14 * 24 * 60 * 60 * 1000, // 14일
     });
   }
 
   // 로그아웃 시 쿠키에서 토큰 삭제
   async logout(res: Response) {
-    res.clearCookie('accessToken');
-    res.clearCookie('refreshToken');
+    res.clearCookie("accessToken");
+    res.clearCookie("refreshToken");
 
     return { success: true };
   }
@@ -86,7 +48,7 @@ export class AuthService {
       const { accessToken } = req.cookies;
 
       const decodedAccessToken = this.jwtService.verify(accessToken, {
-        secret: this.configService.get('JWT_SECRET'),
+        secret: this.configService.get("JWT_SECRET"),
       });
 
       // 검증 후 사용자 정보 반환 -> 가드에서 Request 객체에 할당
@@ -95,7 +57,7 @@ export class AuthService {
         name: decodedAccessToken.name,
       };
     } catch {
-      throw new UnauthorizedException('유효하지 않은 토큰입니다.');
+      throw new UnauthorizedException("유효하지 않은 토큰입니다.");
     }
   }
 
@@ -104,27 +66,27 @@ export class AuthService {
     try {
       const { refreshToken } = req.cookies;
       if (!refreshToken) {
-        throw new UnauthorizedException('유효하지 않은 토큰입니다.');
+        throw new UnauthorizedException("유효하지 않은 토큰입니다.");
       }
 
       const decodedRefreshToken = this.jwtService.verify(refreshToken, {
-        secret: this.configService.get('JWT_SECRET'),
+        secret: this.configService.get("JWT_SECRET"),
       });
       if (!decodedRefreshToken) {
-        throw new UnauthorizedException('유효하지 않은 토큰입니다.');
+        throw new UnauthorizedException("유효하지 않은 토큰입니다.");
       }
 
       const userId = decodedRefreshToken.sub;
 
       const accessToken = this.jwtService.sign(
         { sub: userId, name: decodedRefreshToken.name },
-        { expiresIn: '5m' },
+        { expiresIn: "5m" },
       );
 
-      res.cookie('accessToken', accessToken, {
+      res.cookie("accessToken", accessToken, {
         httpOnly: true,
-        secure: this.configService.get<string>('NODE_ENV') === 'production',
-        sameSite: 'lax',
+        secure: this.configService.get<string>("NODE_ENV") === "production",
+        sameSite: "lax",
         maxAge: 5 * 60 * 1000,
       });
 
@@ -134,7 +96,7 @@ export class AuthService {
         name: decodedRefreshToken.name,
       };
     } catch {
-      throw new UnauthorizedException('유효하지 않은 토큰입니다.');
+      throw new UnauthorizedException("유효하지 않은 토큰입니다.");
     }
   }
 
