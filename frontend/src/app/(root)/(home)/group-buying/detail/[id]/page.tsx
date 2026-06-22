b9e11d9d7642bd1c075d2c3d90336be2f763fa5e
yssson <sonyeseul63@gmail.com>
2026-06-22 20:56:46 +0900

    feat[#25] - 에러/예외 처리 추가

--- frontend/src/app/(root)/(home)/group-buying/detail/[id]/page.tsx
+++ frontend/src/app/(root)/(home)/group-buying/detail/[id]/page.tsx
@@ -1,16 +1,18 @@
 import { getGroupBuyingByIdServer } from "@/apis/services/groupBuying.server";
 import { getParticipantListServer } from "@/apis/services/participant.server";
 import DetailClientPage from "@/app/(root)/(home)/group-buying/detail/[id]/components/DetailClientPage";
+import { notFound } from "next/navigation";
 
 interface PageProps {
   params: { id: string };
 }
 
 export default async function GroupBuyingDetailPage({ params }: PageProps) {
-  const item = await getGroupBuyingByIdServer(params.id);
-  const participantsResult = await getParticipantListServer(params.id);
-
-  const participants = participantsResult?.items || [];
-
-  return <DetailClientPage item={item} participants={participants} />;
+  try {
+    const item = await getGroupBuyingByIdServer(params.id);
+    const participantsResult = await getParticipantListServer(params.id);
+    return <DetailClientPage item={item} participants={participantsResult?.items || []} />;
+  } catch {
+    notFound();
+  }
 }
